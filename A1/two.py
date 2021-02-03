89ad07e2b7ac6ec79af400f99fe6c883fada1bd1
Lance Gomes <lvgomes@edu.uwaterloo.ca>
2021-02-03 03:38:18 -0500

    Question 2

--- A1/two.py
+++ A1/two.py
@@ -6,18 +6,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def sgn(x):
-  if x <= 0:
-    return -1
-  else:
+  if x > 0:
     return 1
-
-def madaline(weights, inputs):
-  pass
-
+  else:
+    return 0
 
 def adaline(weights, inputs):
-  
+  return sgn(np.dot(weights, inputs))
 
+def madaline(inputs):
 
+  # Add bias weight of 1
+  input_a1 = inputs + [1]
+  input_a2 = inputs + [1]
+
+  sgn_1 = adaline(np.array([-1, 1, 0.5]), np.array(input_a1))
+  sgn_2 = adaline(np.array([-1, 1, -0.5]), np.array(input_a2))
+
+  weights = np.array([2, -2, -1])
+  inputs = np.array([sgn_1, sgn_2, 1])
+
+  return adaline(weights, inputs)
+
+if __name__ == "__main__":
+  print(madaline([0,0]))
+  print(madaline([0,1]))
+  print(madaline([1,0]))
+  print(madaline([1,1]))
 
 

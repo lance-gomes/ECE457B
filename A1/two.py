a719619077fa1d7d3076e89155cab48bb2ae505d
Lance Gomes <lvgomes@edu.uwaterloo.ca>
2021-02-06 23:43:23 -0500

    Updates

--- A1/two.py
+++ A1/two.py
@@ -1,7 +1,6 @@
 import random
 import time
 import math
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,7 +8,7 @@ def sgn(x):
   if x > 0:
     return 1
   else:
-    return 0
+    return -1
 
 def adaline(weights, inputs):
   return sgn(np.dot(weights, inputs))
@@ -23,15 +22,39 @@ def madaline(inputs):
   sgn_1 = adaline(np.array([-1, 1, 0.5]), np.array(input_a1))
   sgn_2 = adaline(np.array([-1, 1, -0.5]), np.array(input_a2))
 
-  weights = np.array([2, -2, -1])
+  weights = np.array([1, -1, -1])
   inputs = np.array([sgn_1, sgn_2, 1])
 
   return adaline(weights, inputs)
 
+def graph():
+  x = [i for i in range(-1, 3)]
+  madaline_x = [i for i in range(0, 2)]
+  y_1 = [x_val - 0.5 for x_val in x]
+  y_2 = [x_val + 0.5 for x_val in x]
+  y_3 = [-x_val + 1 for x_val in madaline_x]
+
+  plt.plot(x, y_1, label="y = x - 0.5")
+  plt.plot(x, y_2, label="y = x + 0.5")
+  plt.plot(madaline_x, y_3, label="y = -x + 1")
+
+  plt.plot(0,0, "ro")
+  plt.plot(1,0, "ro")
+  plt.plot(0,1, "ro")
+  plt.plot(1,1, "ro")
+
+  plt.xlabel('X')
+  plt.ylabel('Y')
+  plt.title('X vs Y')
+  plt.legend()
+  plt.show()
+
+
 if __name__ == "__main__":
-  print(madaline([0,0]))
-  print(madaline([0,1]))
-  print(madaline([1,0]))
+  print(madaline([-1,-1]))
+  print(madaline([-1,1]))
+  print(madaline([1,-1]))
   print(madaline([1,1]))
+  graph()
 
 
